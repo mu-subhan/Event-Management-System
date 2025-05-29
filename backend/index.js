a7d98ed76e412bb3b2288eec14bd6560cdf47b49
usamarazzaq90 <usamarazzaq9014@gmail.com>
2025-05-29 16:47:01 +0500

    commit

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
